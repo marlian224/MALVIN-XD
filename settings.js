ed1d8a77628c97798618700d4ba03e55a1343e1b
marlian224 <maliannawal@gmail.com>
2025-05-25 07:21:05 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id make sure it starts with malvin~
+// add your malvin~R2EGXIaa#BQORBgrEtAGIsVIVoVNKXDtDWFUJExAPAHXYutqtGOoSession Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "ᴍᴀʟᴠɪɴ-xᴅ",
